1e4781d04f9ae740d64de11b2c3bcc6db3c6fe6f
youms <yussef.msabeh@gmail.com>
2022-07-03 20:36:51 +0300

    taBbar widget

---
+++ lib/HomPage.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:tap_bar/Screens/Favorite.dart';
+import 'package:tap_bar/Screens/HomePageScreen.dart';
+import 'package:tap_bar/Screens/Profile.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(physics: ScrollPhysics(), tabs: [
+            Tab(child: Text("Home")),
+            Tab(child: Text("Favorite")),
+            Tab(child: Text("profile"))
+          ]),
+        ),
+        body: TabBarView(children: [HomePageScreen(), Favorite(), Profile()]),
+      ),
+    );
+  }
+}
